fa7a61ecfa9d5fec79c157b013b43c1237ce2d75
M7MD <mohamed.hassan_1996122m@yahoo.com>
2017-04-09 09:04:16 -0700

    updated

--- cachematrix.R
+++ cachematrix.R
@@ -1,17 +1,31 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
+## calculating and caching the inverse of a matrix 
+
 ## Write a short comment describing this function
 
+## getting the matrix
 makeCacheMatrix <- function(x = matrix()) {
+  ## initially assigning the inverse to NULL
   s <- NULL
+  
+  ## to set the value of the matrix
   set <- function(y) {
     x <<- y
     s <<- NULL
   }
+  
+  ## to get the value of the matrix
   get <- function() x
+  
+  ## to set the value of the inverse
   setsolve <- function(solve) s <<- solve
+  
+  ## to get the value of the inverse
   getsolve <- function() s
+  
+  ## our container ^_^
   list(set = set, get = get,
        setsolve = setsolve,
        getsolve = getsolve)
@@ -19,12 +33,17 @@ makeCacheMatrix <- function(x = matrix()) {
 
 ## Write a short comment describing this function
 
+## getting the container
 cacheSolve <- function(x, ...) {
+  
+  ## checking the inverse existance
   s <- x$getsolve()
   if(!is.null(s)) {
     message("getting cached data")
     return(s)
   }
+  
+  ## calculating and caching the inverse
   data <- x$get()
   s <- solve(data, ...)
   x$setsolve(s)
